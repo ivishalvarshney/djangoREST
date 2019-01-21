a20a2452fdf3017396227d309986699af1f7930e
Vishal Varshney <vishal.varshney@incedoinc.com>
2019-01-21 13:54:30 +0530

    add defaul django-restframework in 'rest' app

--- rest/views.py
+++ rest/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the REST index.")
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the REST index.")
+class users(APIView):
+	"""This API is used to fetch the detail of heart rate of user"""
+	def get(self, request):
+		resp =[
+			{"id":1,"firstname":"Antoine","lastname":"Kaufman","phone":"976842345"},
+			{"id":2,"firstname":"Raphael","lastname":"Wallace","phone":"34564"},
+		]
+		return Response(resp)
+
+	def put(self, request):
+		resp =[
+			{"id":1,"firstname":"Antoine","lastname":"Kaufman","phone":"976842345"},
+			{"id":2,"firstname":"Raphael","lastname":"Wallace","phone":"34564"},
+		]
+		return Response(resp)
